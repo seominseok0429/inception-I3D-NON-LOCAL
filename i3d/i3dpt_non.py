017bb8f998fe5ce5fc33d6691ff0c8ebdb03f97c
seominseok <33244972+seominseok0429@users.noreply.github.com>
2019-10-08 09:34:50 +0900

    add WS

--- i3d/i3dpt_non.py
+++ i3d/i3dpt_non.py
@@ -8,7 +8,17 @@ import numpy as np
 import torch
 from torch.nn import ReplicationPad3d
 
-
+class Conv3d(nn.Conv3d):
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0, dilation=1, groups=1, bias=True):
+        super(Conv3d, self).__init__(in_channels, out_channels, kernel_size, stride, padding, dilation, groups, bias)
+    
+    def forward(self, x):
+        weight = self.weight
+        weight_mean = weight.mean(dim=1, keepdim=True).mean(dim=2, keepdim=True).mean(dim=3, keepdim=True).mean(dim=4, keepdim=True)
+        weight = weight - weight_mean
+        std = weight.view(weight.size(0), -1).std(dim=1).view(-1, 1, 1, 1, 1) + 1e-5 
+        weight = weight / std.expand_as(weight)
+        return F.conv3d(x, weight, self.bias, self.stride, self.padding, self.dilation, self.groups)
 
 def get_padding_shape(filter_shape, stride):
     def _pad_top_bottom(filter_dim, stride_val):
